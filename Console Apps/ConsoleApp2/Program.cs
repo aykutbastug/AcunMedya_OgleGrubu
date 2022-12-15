8947daaae52346a42042d8f863615525cc3dc1b2
Aykut Baştuğ <aykut.bastug@codeove.com>
2022-12-15 18:21:19 +0300

    etüt örnekleri

---
+++ Console Apps/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kullaniciAdi = "aykut";
+            string sifre = "123";
+
+            Console.WriteLine("Kullanıcı Adınızı Giriniz :");
+            string _kullaniciAdi = Console.ReadLine();
+
+            Console.WriteLine("Şifrenizi giriniz :");
+            string _sifre = Console.ReadLine();
+
+            if (kullaniciAdi == _kullaniciAdi && sifre == _sifre)
+            {
+                Console.WriteLine("Giriş başarılı");
+            }
+            else
+                Console.WriteLine("Kullanıcı adı veya şifresi hatalı.");
+        }
+    }
+}
---
+++ Console Apps/EtutOrnekler/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace EtutOrnekler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Sayıyı Girin:
+            //1. sayıyı alalım
+            //2. Sayıyı Girin:
+            //2. sayıyı alalım
+            //Yapmak istediğiniz işlem nedir ? (1=Toplamam, 2=Çarpma, 3=Çıkarma, 4=Bölme)
+            //seçime göre işlemi yapıp sonucu ekranda gösterelim
+
+            try
+            {
+                //hata alınma ihtimali olan kod blogları buraya yazılır.
+            }
+            catch (Exception hataMesaji)
+            {
+                //hata alındığında çalışmasını istediğimiz kodlar buraya yazılır
+            }
+            finally
+            {
+                //hata alınsa da alınması her halukarda çalışmasını istediğiniz kodları buraya yazabilirsiniz
+            }
+
+
+            int sayi1 = 0;
+            int sayi2 = 0;
+
+            try
+            {
+                Console.WriteLine("1. Sayıyı Giriniz :");
+                sayi1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("2. Sayıyı Giriniz :");
+                sayi2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Yapmak istediğiniz işlemi seçin.\n(1=Toplamam, 2=Çarpma, 3=Çıkarma, 4=Bölme)");
+                string islem = Console.ReadLine();
+                int sonuc = 0;
+
+                switch (islem)
+                {
+                    case "1":
+                        sonuc = sayi1 + sayi2;
+                        break;
+
+                    case "2":
+                        sonuc = sayi1 * sayi2;
+                        break;
+
+                    case "3":
+                        sonuc = sayi1 - sayi2;
+                        break;
+
+                    case "4":
+                        sonuc = sayi1 / sayi2;
+                        break;
+
+                    default:
+                        Console.WriteLine("Hatalı bir seçim yaptınız..");
+                        break;
+                }
+
+                Console.WriteLine("İşlem Sonucu = " + sonuc);
+            }
+            catch (Exception hata)
+            {
+                Console.WriteLine("İşlem sırasında hata oluştu. ("+hata.Message+")");
+                Console.WriteLine(hata.StackTrace);
+            }          
+
+        }
+    }
+}
