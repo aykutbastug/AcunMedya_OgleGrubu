be9b8a14f165c1566655db8c87efa2f5f7081467
Aykut Baştuğ <aykut.bastug@codeove.com>
2023-01-02 17:52:09 +0300

    MVC örnekleri

---
+++ ASP.NET Core MVC/WebApplication1/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            Ogrenci ogrenci = new Ogrenci();
+            ogrenci.Id = 1;
+            ogrenci.Adi = "Aykut";
+            ogrenci.Soyadi = "BAŞTUĞ";
+
+
+            Ogrenci ogrenci2 = new Ogrenci();
+            ogrenci2.Id = 2;
+            ogrenci2.Adi = "Aykut2";
+            ogrenci2.Soyadi = "BAŞTUĞ2";
+
+            List<Ogrenci> ogrenciler = new List<Ogrenci>();
+            ogrenciler.Add(ogrenci);
+            ogrenciler.Add(ogrenci2);
+
+            return View(ogrenciler);
+        }
+
+        public IActionResult  Login()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ASP.NET Core MVC/WebApplication1/Models/Ogrenci.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class Ogrenci
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }    
+    }
+}
