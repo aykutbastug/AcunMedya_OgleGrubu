27586da42104a57046f47f170f49f9bd86c9f157
AYKUT BAŞTUĞ <aykutbastug@gmail.com>
2023-04-18 17:17:26 +0300

    .

---
+++ ASP.NET Core MVC/CrmUygulamasi/CrmUygulamasi.UI/Controllers/SupplierController.cs
@@ -0,0 +1,52 @@
+﻿using CrmUygulamasi.BLL;
+using CrmUygulamasi.DAL.EntityFramework;
+using CrmUygulamasi.Entites;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrmUygulamasi.UI.Controllers
+{
+    public class SupplierController : Controller
+    {
+        SupplierManager supplierManager = new SupplierManager(new EfSupplierRepository());
+        public IActionResult Index()
+        {
+            List<Supplier> list = supplierManager.ListAll();
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Supplier supplier)
+        {
+            supplierManager.Create(supplier);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult EditSupplierPartial(int id)
+        {
+            Supplier supplier = supplierManager.Get(id);
+
+            return PartialView("_EditSupplierPartialView", supplier);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Supplier supplier)
+        {
+            supplierManager.Update(supplier);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult DeleteSupplierPartial(int id)
+        {
+            Supplier supplier = supplierManager.Get(id);
+
+            return PartialView("_DeleteSupplierPartialView", supplier);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Supplier supplier)
+        {
+            supplierManager.Delete(supplier);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
